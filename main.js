2fc231ed3a16a75c69c6d55b780d8fddadd75c80
Damaris Zambrano <111249412+Dada0131@users.noreply.github.com>
2022-08-28 19:01:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const contenido = document.getElementById("contenido");
+
+let datos = "Practica de HTML, CSS Y JS";
+contenido.innerHTML = "<h2>"+datos+"</h2>";
+
+
+
+let nombre ="Damaris Zambrano";
+
+const COLOR_ROJO = "#FF000"
+
+let resultado = "Nombre: " + nombre;
+
+alert(resultado);
+
+nombre = "Ingeniería en Software, 3er Semestre"
+
+alert(nombre)
